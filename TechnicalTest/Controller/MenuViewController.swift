8f48225e575e36771c862c09bd203753f4c915d4
Md. Kamrul Hassan Sabuj <kamrulhassansabuj@gmail.com>
2019-06-15 19:43:28 +0600

    Side menu added

---
+++ TechnicalTest/Controller/MenuViewController.swift
@@ -0,0 +1,134 @@
+//
+//  MenuViewController.swift
+//  TechnicalTest
+//
+//  Created by Interview AITS on 15/6/19.
+//  Copyright © 2019 Interview AITS. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+protocol SlideMenuDelegate {
+    func slideMenuItemSelectedAtIndex(_ index : Int32)
+}
+
+class MenuViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    /**
+     *  Array to display menu options
+     @IBOutlet weak var versionLabel: UILabel!
+     */
+    
+    /**
+     *  Transparent button to hide menu
+     */
+    @IBOutlet var btnCloseMenuOverlay : UIButton!
+    
+    @IBOutlet weak var tblMenuOptions: UITableView!
+    
+    /**
+     *  Array containing menu options
+     */
+    var arrayMenuOptions = [Dictionary<String,String>]()
+    
+    /**
+     *  Menu button which was tapped to display the menu
+     */
+    var btnMenu : UIButton!
+    
+    /**
+     *  Delegate of the MenuVC
+     */
+    var delegate : SlideMenuDelegate?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tblMenuOptions.tableFooterView = UIView()
+        tblMenuOptions.rowHeight = 45
+        
+        //        tblMenuOptions.separatorColor = UIColor(red:0.22, green:0.38, blue:0.40, alpha:1.0)
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        updateArrayMenuOptions()
+    }
+    
+    func updateArrayMenuOptions(){
+        arrayMenuOptions.append(["title":"SignIn/SignUp", "icon":""])
+        arrayMenuOptions.append(["title":"Weekend Offers!", "icon":""])
+        arrayMenuOptions.append(["title":"Mega Offer", "icon":""])
+        arrayMenuOptions.append(["title":"Dairy-Bakery", "icon":"rsz_analysis_grey-01 (1)"])
+        arrayMenuOptions.append(["title":"Bevarages", "icon":""])
+        arrayMenuOptions.append(["title":"General Groceries", "icon":""])
+        arrayMenuOptions.append(["title":"Baby & Toddler","icon":""])
+        arrayMenuOptions.append(["title":"Pet Food & Supplies","icon":""])
+        arrayMenuOptions.append(["title":"Personal Care","icon":""])
+        arrayMenuOptions.append(["title":"Help","icon":""])
+        arrayMenuOptions.append(["title":"Contact Us","icon":""])
+        arrayMenuOptions.append(["title":"Privacy Policy","icon":""])
+        arrayMenuOptions.append(["title":"Terms & Condition","icon":""])
+        
+        tblMenuOptions.reloadData()
+    }
+    
+    @IBAction func onCloseMenuClick(_ button:UIButton!){
+        btnMenu.tag = 0
+        
+        if (self.delegate != nil) {
+            var index = Int32(button.tag)
+            if(button == self.btnCloseMenuOverlay){
+                index = -1
+            }
+            delegate?.slideMenuItemSelectedAtIndex(index)
+        }
+        
+        UIView.animate(withDuration: 0.3, animations: { () -> Void in
+            self.view.frame = CGRect(x: -UIScreen.main.bounds.size.width, y: 0, width: UIScreen.main.bounds.size.width,height: UIScreen.main.bounds.size.height)
+            self.view.layoutIfNeeded()
+            self.view.backgroundColor = UIColor.clear
+        }, completion: { (finished) -> Void in
+            self.view.removeFromSuperview()
+            self.removeFromParent()
+        })
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell : UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "cellMenu")!
+        
+        cell.selectionStyle = UITableViewCell.SelectionStyle.none
+        cell.layoutMargins = UIEdgeInsets.zero
+        cell.preservesSuperviewLayoutMargins = false
+        cell.backgroundColor = UIColor.clear
+        
+        let lblTitle : UILabel = cell.contentView.viewWithTag(101) as! UILabel
+        let imgIcon : UIImageView = cell.contentView.viewWithTag(100) as! UIImageView
+        
+        imgIcon.image = UIImage(named: arrayMenuOptions[indexPath.row]["icon"]!)
+        lblTitle.text = arrayMenuOptions[indexPath.row]["title"]!
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let btn = UIButton(type: UIButton.ButtonType.custom)
+        btn.tag = indexPath.row
+        self.onCloseMenuClick(btn)
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return arrayMenuOptions.count
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1;
+    }
+}
