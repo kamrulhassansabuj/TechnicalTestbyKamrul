9b7c7292be4be510d2f99fd8f631d5ee66665b65
Imran <hossain.imran.cse11@gmail.com>
2019-06-15 10:46:08 +0600

    Initial Commit

---
+++ TechnicalTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TechnicalTest
+//
+//  Created by Interview AITS on 15/6/19.
+//  Copyright © 2019 Interview AITS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
